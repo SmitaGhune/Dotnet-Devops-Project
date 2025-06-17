bc010c318617ad27b0cadd432083c8a8f48feb1f
Smita Ghune <72501177+9607491581@users.noreply.github.com>
2025-06-17 11:13:48 +0530

    New Change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,31 @@
 variable "resource_group_name" {
-  default = "dotnetapp-rg"
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "dotnetapp-rg"
 }
 
 variable "location" {
-  default = "East US"
+  description = "Azure region for all resources"
+  type        = string
+  default     = "East US"
+  validation {
+    condition     = contains(["East US", "West US", "Central India", "East US 2"], var.location)
+    error_message = "Location must be a valid Azure region like 'East US', 'East US 2', etc."
+  }
 }
 
 variable "acr_name" {
-  default = "dotnetacr123"
+  description = "Name of the Azure Container Registry"
+  type        = string
+  default     = "dotnetacr123"
+  validation {
+    condition     = length(var.acr_name) <= 50 && can(regex("^[a-zA-Z0-9]+$", var.acr_name))
+    error_message = "ACR name must be alphanumeric and <= 50 characters."
+  }
 }
 
 variable "aks_name" {
-  default = "dotnet-aks-cluster"
+  description = "Name of the Azure Kubernetes Service cluster"
+  type        = string
+  default     = "dotnet-aks-cluster"
 }

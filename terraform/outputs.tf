923574fa4003b212b1d52e624cfe106e7776ab11
Smita Ghune <72501177+9607491581@users.noreply.github.com>
2025-06-17 11:14:49 +0530

    New Change

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  description = "The login server URL of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
 }
 
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  description = "The name of the Azure Kubernetes Service (AKS) cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
 }
